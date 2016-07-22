9972516579327b7474049a91c3c3bfa679280cd6
Elliot Hawkins <elliotshawkins@gmail.com>
2016-07-22 08:03:18 -0700

    Fix commented out position movement in example mouse script

--- freepie example mouse script.py
+++ freepie example mouse script.py
@@ -12,16 +12,16 @@ def update():
 	mouse.middleButton = joystick[0].getDown(15)
 	
 	#Mouse movement using position data
-	#mouse.deltaX = -1 * freePieIO[1].yaw#(freePieIO[2].x - lastX) * cursorScale#(freePieIO[0].x - lastX) * cursorScale
-	#mouse.deltaY = -1 * freePieIO[1].pitch#-1 * (freePieIO[0].y - lastY) * cursorScale
+	#mouse.deltaX = (freePieIO[0].x - lastX) * cursorScale
+	#mouse.deltaY = -1 * (freePieIO[0].y - lastY) * cursorScale
 	#lastX = freePieIO[0].x
 	#lastY = freePieIO[0].y
 	
 	#Mouse movement using Gryoscope
-	mouse.deltaX = -1 * freePieIO[1].yaw
-	mouse.deltaY = -1 * freePieIO[1].pitch
-	lastX = 0
-	lastY = 0
+	#mouse.deltaX = -1 * freePieIO[1].yaw
+	#mouse.deltaY = -1 * freePieIO[1].pitch
+	#lastX = 0
+	#lastY = 0
 	
 if starting:
 	lastX = freePieIO[0].x
