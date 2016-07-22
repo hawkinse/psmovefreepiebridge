a9a8315a3c68457587f290dea1eb858df74cf50f
Elliot Hawkins <elliotshawkins@gmail.com>
2016-07-22 07:44:34 -0700

    Mouse example script now uses gyroscope for movement by default instead of camera position as per request

--- freepie example mouse script.py
+++ freepie example mouse script.py
@@ -3,7 +3,6 @@ def update():
 	global lastX
 	global lastY
 	global cursorScale
-	global wheelVal
 	
 	#bind left mouse to trigger button
 	#Right mouse to Move button
@@ -12,10 +11,17 @@ def update():
 	mouse.rightButton = joystick[0].getDown(19)
 	mouse.middleButton = joystick[0].getDown(15)
 	
-	mouse.deltaX = (freePieIO[0].x - lastX) * cursorScale
-	mouse.deltaY = -1 * (freePieIO[0].y - lastY) * cursorScale
-	lastX = freePieIO[0].x
-	lastY = freePieIO[0].y
+	#Mouse movement using position data
+	#mouse.deltaX = -1 * freePieIO[1].yaw#(freePieIO[2].x - lastX) * cursorScale#(freePieIO[0].x - lastX) * cursorScale
+	#mouse.deltaY = -1 * freePieIO[1].pitch#-1 * (freePieIO[0].y - lastY) * cursorScale
+	#lastX = freePieIO[0].x
+	#lastY = freePieIO[0].y
+	
+	#Mouse movement using Gryoscope
+	mouse.deltaX = -1 * freePieIO[1].yaw
+	mouse.deltaY = -1 * freePieIO[1].pitch
+	lastX = 0
+	lastY = 0
 	
 if starting:
 	lastX = freePieIO[0].x
