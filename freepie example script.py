6cc965965899b9b206edf3c9cc18a55a97255ef4
Elliot Hawkins <elliotshawkins@gmail.com>
2016-08-31 01:06:38 -0700

    Remove commented out diagnostics in sample freepie script

--- freepie example script.py
+++ freepie example script.py
@@ -7,12 +7,7 @@ def update():
 	freeTrack.x = (freePieIO[0].x * 10)
 	freeTrack.y = (freePieIO[0].y * 10)
 	freeTrack.z = (freePieIO[0].z * 10)
-	#diagnostics.watch(freeTrack.x)
-	#diagnostics.watch(freeTrack.y)
-	#diagnostics.watch(freeTrack.z)
-	#diagnostics.watch(freeTrack.pitch)
-	#diagnostics.watch(freeTrack.roll)
-	#diagnostics.watch(freeTrack.yaw)
+
 	
 if starting:
 	freePieIO[0].update += update
