c619e712599cde6c44a012ed46d10652343972fa
Elliot Hawkins <elliotshawkins@gmail.com>
2017-01-07 18:54:47 -0800

    Add example batch file that uses command line arguments to track controller ID 2, which is commonly the third controller.

---
+++ TrackThirdControllerExample.bat
@@ -0,0 +1,8 @@
+:: PSMoveFreepieBridge.exe -t <ID 1> <ID 2> ... -c <Color for ID 1> <Color for ID 2>...
+
+:: -c flag is optional. Colors use the numbers listed in the prompt when ran without arguments, use -1
+:: for its default color (or previously used color, if set prior). Colors are assigned to the ID in the 
+:: same position on the -t flag.
+
+::The following tracks what is commonly the third controller, with the default color for that controller.
+PSMoveFreepieBridge.exe -t 2 -c -1
