a45ab0fd15eb2c5561d9d9dd73545e61531e42da
Elliot Hawkins <elliotshawkins@gmail.com>
2017-01-07 18:38:09 -0800

    Add command line arguments. Numbers following -t specify Tracked controller IDs. Numbers following -c specify the Color of each controller, with -1 being default color. Example: Tracking controller IDs 0 and 2, with ID 0 being red and 2 being default would be: "PSMoveFreepieBridge.exe -t 0 2 -c 3 -1"

--- PSMoveFreepieBridge/PSMoveFreepieBridge.cpp
+++ PSMoveFreepieBridge/PSMoveFreepieBridge.cpp
@@ -1,21 +1,16 @@
 #include "stdafx.h"
 #include "FreepieMoveClient.h"
 
-int main()
-{
-	int32_t controllerCount = 0;
+void prompt_arguments(int32_t &controllerCount, int32_t* controllerIDs, int32_t* bulbColors) {
 	std::cout << "How many controllers do you want to track (1-4)? Note that more than 1 disables raw sensor data access and 4 disables button access: ";
 	std::cin >> controllerCount;
 
-	int32_t controllerIDs[4];
-	int32_t freepieIndicies[4] = { 0, 1, 2, 3 };
-	int32_t bulbColors[4];
-	for (int i = 0; i < controllerCount; i++) 
+	for (int i = 0; i < controllerCount; i++)
 	{
 		int32_t controllerID = 0;
 		std::cout << "Enter the ID of the controller you wish to use: ";
 		std::cin >> controllerID;
-		
+
 		int32_t bulbColor = -1;
 		char customColorChoice;
 		std::cout << "Do you want to use a custom bulb color for this controller (y/n)? ";
@@ -35,9 +30,81 @@ int main()
 		controllerIDs[i] = controllerID;
 		bulbColors[i] = bulbColor;
 	}
+}
 
-	FreepieMoveClient* client = new FreepieMoveClient();
-	client->run(controllerCount, controllerIDs, bulbColors, freepieIndicies, controllerCount < 2);
+bool parse_arguments(int argc, char** argv, int32_t &controllerCount, int32_t* controllerIDs, int32_t* bulbColors) {
+	bool bSuccess = true;
+
+	int index = 1;
+	while (index < argc) {
+		if ((strcmp(argv[index], "-t") == 0) && controllerCount < 1) {
+			index++;
+
+			//All numeric values after the -t flag are controller IDs. Loop through and add them.
+			while ((index < argc) && isdigit(*argv[index])) {
+				//Only add up to four controllers
+				if (controllerCount < 4) {
+					controllerIDs[controllerCount] = atoi(argv[index]);
+					controllerCount++;
+				}
+				else {
+					std::cout << "More than four controllers have been specified on the command line!" << std::endl;
+					bSuccess = false;
+				}
+
+				index++;
+			}
+		}
+		else if (strcmp(argv[index], "-c") == 0) {
+			index++;
+			int32_t colorIndex = 0;
+			//All numeric values after the -c flag are color indicies
+			while ((index < argc) && (isdigit(*argv[index]) || (strcmp(argv[index], "-1") == 0))) {
+				//Only add up to four controller indicies
+				if (colorIndex < 4) {
+					bulbColors[colorIndex] = atoi(argv[index]);
+					colorIndex++;
+				}
+				else {
+					std::cout << "More than four colors have been specified on the command line!" << std::endl;
+					bSuccess = false;
+				}
+
+				index++;
+			}
+		}
+		else {
+			std::cout << "Unrecognized command line argument " << argv[index] << std::endl;
+			bSuccess = false;
+			break;
+		}
+	}
+
+	return bSuccess;
+}
+
+int main(int argc, char** argv)
+{
+	int32_t controllerCount = 0;
+	int32_t controllerIDs[4];
+	int32_t freepieIndicies[4] = { 0, 1, 2, 3 };
+	int32_t bulbColors[4] = { -1, -1, -1, -1 };
+	bool bRun = true;
+
+	if (argc == 1) {
+		prompt_arguments(controllerCount, controllerIDs, bulbColors);
+	}
+	else {
+		if (!parse_arguments(argc, argv, controllerCount, controllerIDs, bulbColors)) {
+			std::cout << "Command line arguments are not valid." << std::endl;
+			bRun = false;;
+		}
+	}
+
+	if (bRun) {
+		FreepieMoveClient* client = new FreepieMoveClient();
+		client->run(controllerCount, controllerIDs, bulbColors, freepieIndicies, controllerCount < 2);
+	}
 
 	std::cout << "PSMoveFreepieBridge has ended" << std::endl;
 	
